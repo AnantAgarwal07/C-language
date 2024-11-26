7cea2ebe2b58fea6fd2139591fefe462bd104d14
Anant Agarwal <myselfanantagarwal@gmail.com>
2024-11-26 15:58:39 +0530

    Revert "Add files via upload"

--- 1-1a.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int l=10,b=5;
-    printf("Area is %d",l*b);
-}
--- 1-1b.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int l,b,area;
-    printf("Enter the length and breadth of the rectangle");
-    scanf("%d %d",&l,&b);
-    area=l*b;
-    printf("The area is %d",area);
-}
