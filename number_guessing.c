78d2cf69f2cb04a9ad89452c6f0ac4d8852073a2
Anant Agarwal <myselfanantagarwal@gmail.com>
2024-11-26 13:03:27 +0530

    number_guessing.c

--- number_guessing.c
+++ number_guessing.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 int main()
 {
+    srand(time(NULL));
     int n=rand()%100,input;
     printf("This aim of this game is for you to guess a random number between 1 & 100. \nEnter a number between 1 & 100 (including 1 & 100) for your guess: ");
     scanf("%d",&input);    
